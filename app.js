ab25e2032c3927b30e0f1f358dcb0ff70f82640c
nds95 <nds95@kakao.com>
2021-12-10 23:15:23 +0900

    commit

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton= document.querySelector("#login-form Button");
 
 function handleLoginBtnClick() {
-    console.log(`Hello, ${loginInput.value}`);
+    let inputValue = loginInput.value;
+
+    if(inputValue === "" || inputValue === " ") {
+        alert("input your name!");
+    } else {
+        console.log(`Hello, ${inputValue}`);
+    }
 }
 
 loginButton.addEventListener("click", handleLoginBtnClick);
